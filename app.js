11b39d6157ffd222ee53b003a5d5ff5455efc335
hannah <contactuser2220@gmail.com>
2020-10-01 15:43:58 -0400

    second commit, connected to database

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const { db } = require("./models");
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.get("/", (req, res, next) => {
-  res.send("hello world");
+  res.send("helloworld");
+});
+
+app.listen(3000, () => {
+  console.log("im listening to port 3000");
 });
