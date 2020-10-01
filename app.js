fa91d05559483fece93013cddd9d62aa1cc79821
Prakruti PK <prakruti.pk@gmail.com>
2020-10-01 16:32:44 -0400

    sequelize essentials complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const { db } = require("./models");
+const { db, User, Page } = require("./models");
 
 db.authenticate().then(() => {
   console.log("connected to the database");
@@ -11,6 +11,20 @@ app.get("/", (req, res, next) => {
   res.send("helloworld");
 });
 
+const init = async() => {
+await db.sync()
+.then (() => {
+console.log('db is synced!')
+},
+await Page.sync()
+.then (() => {
+console.log('Page is synced!')
+},
+await User.sync()
+.then (() => {
+  console.log("we're connected to the User!")
+},
 app.listen(3000, () => {
   console.log("im listening to port 3000");
 });
+}
--- models/index.js
+++ models/index.js
@@ -1,6 +1,39 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/wikistack");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {logging: false});
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }, 
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  },//Enum is a constructor; either an array of values or an object. 
+})
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+
 
 module.exports = {
   db,
+  Page,
+  User
 };
